51706a75742818f2b0af706c9498cd2b1543342c
Noah Prince <noahprince8@gmail.com>
2016-06-01 14:04:14 -0500

    Initial Commit

---
+++ Bardar/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Bardar
+//
+//  Created by Noah Prince on 6/1/16.
+//  Copyright © 2016 Noah Prince. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
